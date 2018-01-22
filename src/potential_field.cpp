1679ff6c8b1ad0000ccaef8b761b4a8e26d612fb
Adarsh <adarsh23.sehgal@gmail.com>
2018-01-22 13:13:34 -0800

    Removed comments from working code

--- src/potential_field.cpp
+++ src/potential_field.cpp
@@ -5,7 +5,6 @@
 #include <geometry_msgs/Point.h>
 #include <tf/transform_listener.h>
 #include <nav_msgs/Odometry.h>
-//#include "Aria.h"
 #include <time.h>
 #include <stdio.h>
 #include <math.h>
@@ -22,10 +21,9 @@ struct OdomData
 class RobotDriver
 {
 	private:
-	  //! The node handle we'll be using
+	  
 	  ros::NodeHandle nh_;
-	  //! We will be publishing to the "cmd_vel" topic to issue commands
-          //ros::Publisher cmd_vel_pub_;
+
 	  ros::Subscriber odom_pub;
           ros::Subscriber virtual_point_sub;
 
@@ -46,7 +44,7 @@ class RobotDriver
           }
           double Angle;
           double RobotHeading;
-          struct OdomData OdomDataSeekur;
+          struct OdomData OdomDataJR2;
           struct OdomData VirtualVehicle;
           bool Wantstotrack;
 
@@ -62,15 +60,15 @@ class RobotDriver
 
 void RobotDriver::OdomCallback(const nav_msgs::Odometry::ConstPtr& odomsg)
 {
-    OdomDataSeekur.x = odomsg->pose.pose.position.x;
-    OdomDataSeekur.y = odomsg->pose.pose.position.y;
+    OdomDataJR2.x = odomsg->pose.pose.position.x;
+    OdomDataJR2.y = odomsg->pose.pose.position.y;
 }
 
 void RobotDriver::VirtualPointCallback(const geometry_msgs::Point::ConstPtr & pointmsg)
 {
     VirtualVehicle.x = pointmsg->x;
     VirtualVehicle.y = pointmsg->y;
-    GoToPoint(OdomDataSeekur,VirtualVehicle);
+    GoToPoint(OdomDataJR2,VirtualVehicle);
 }
 
 //***********MAIN PROGRAM OF THE PATH PLANNING USING ODOMETRY**************
@@ -102,9 +100,7 @@ int main(int argc, char** argv)
             tf::StampedTransform transform;
             // find out the heading of the target
             try{
-              //listener.lookupTransform("/virtual_vehicle", "/virtual_vehicle2",
-              //                         ros::Time(0), transform);
-//ROS_INFO("I'm here");
+              
 	      listener.lookupTransform("/VirtualTarget", "/world",
                                        ros::Time(0), transform);
 
@@ -126,7 +122,7 @@ int main(int argc, char** argv)
 
                }
             catch (tf::TransformException ex){
-		//std::cout<<"I'm here";
+		
               ROS_ERROR("%s",ex.what());
             }
             relative_heading_seekur_target = atan2(transform.getOrigin().y(),
@@ -147,28 +143,10 @@ int main(int argc, char** argv)
             b = Pr;
             debug = asin(a);
             Theta_R = relative_heading_seekur_target + asin(a/b);
-            //ROS_INFO("Theta_R: %f Theta_mt: %f Phi: %f debug: %f distance %f \n", Theta_R,heading_target,relative_heading_seekur_target,debug,relative_distance_seekur_target);
-            //ROS_INFO("%f %f %f %f \n", heading_target,distance_fake,relative_heading_seekur_target,relative_distance_seekur_target);
+            
             heading_input = Theta_R/3;
             speed_input = 0.11;
 
-            // For safety reason, if the virtual vehicle behind the robot..stop tracking
-            // relative_heading_seekur_target -0.8 rad n 0.8 are the view of the robot
-
-            /*if (relative_heading_seekur_target < -0.7 || relative_heading_seekur_target > 0.7)
-            {
-                ROS_INFO("target is behind the robot! No Tracking!!!");
-                heading_input = 0;
-                speed_input = 0;
-            }
-
-            // The goal!
-            if (relative_distance_seekur_target < 0.1)
-            {
-                ROS_INFO("distance of robot n target is close..around 50 cm");
-                speed_input = 0;
-                heading_input = 0;
-            }*/
             // For safety reason, the maximum angular speed is 0.15;
             if (heading_input < -0.15 || heading_input > 0.15)
             {
@@ -187,26 +165,11 @@ int main(int argc, char** argv)
             base_cmd.angular.z = heading_input;
             base_cmd.linear.x = speed_input;
 	    
-	    //std::cout<<heading_input;
-
-            /*
-            if (relative_distance_seekur_target < 0.01)
-            {
-                base_cmd.linear.x = 0;
-            }
-            */
-            driver.cmd_vel_pub_.publish(base_cmd);
-            //rate.sleep();
-
-
-
+	    driver.cmd_vel_pub_.publish(base_cmd);
+            
             rate.sleep();
             ros::spinOnce();
           }
 
-
-          //ros::spin();
-         // ros::Subscriber sub = nh.subscribe("/RosAria/pose", 1000, driver.OdomCallback,this)
-         //ros::spin();
 }
 
